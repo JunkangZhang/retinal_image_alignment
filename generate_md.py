c3578135c2ba7c987ddf27e8caf1c3369472943e
junkangzhang <viking1415926535@gmail.com>
2022-09-20 12:41:35 -0700

    add test page

--- generate_md.py
+++ generate_md.py
@@ -1,3 +1,19 @@
-import numpy as np
 import bibtexparser
 import json
+import yaml
+import textwrap
+
+def write_frontmatter(fp):
+    text = '''---\npermalink: /test/\n---\n'''
+    fp.write(textwrap.dedent(text))
+
+
+if __name__=='__main__':
+    bibtex_dict = bibtexparser.load(open("_data/reference.bib")).entries_dict
+    reference_json = json.load(open('_data/reference.json'))
+    people_yaml = yaml.load(open('_data/people.yml'))
+    print(' ')
+
+    fp_w = open('test.html', 'w')
+    write_frontmatter(fp_w)
+    fp_w.write('<h2 id="test-page">Test page</h2>\n')
--- test.md
+++
@@ -1,5 +0,0 @@
----
-permalink: /test/
----
-
-## Test page
